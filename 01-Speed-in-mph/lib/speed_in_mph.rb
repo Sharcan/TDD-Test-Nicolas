d8a913e860034aba90dabef7a8f69b42c05dbb83
Brazzolotto Nicolas <nicolas.brazzolotto@edu.devinci.fr>
2022-08-23 14:51:03 +0200

    first exercise

--- 01-Speed-in-mph/lib/speed_in_mph.rb
+++ 01-Speed-in-mph/lib/speed_in_mph.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 # TO DO: write a method that takes a float as an argument and
 # returns it converted from kph (km/h) to mph (mi/h)
+def speed_in_mph(speed)
+  return 'Please pass a valid speed.' unless speed.is_a?(Numeric)
+
+  mph = 0.6213711922
+  new_speed_value = speed * mph
+  new_speed_value.round(2)
+end
--- 01-Speed-in-mph/spec/speed_in_mph_spec.rb
+++ 01-Speed-in-mph/spec/speed_in_mph_spec.rb
@@ -1,2 +1,19 @@
+# frozen_string_literal: true
+
 # TO DO: Write the specs that ensures our speed_in_mph method
 # successfully converts speed from km/h to mi/h.
+
+require 'speed_in_mph'
+
+describe '#speed_in_mph' do
+  it 'returns an error if the parameter is not an Number' do
+    error = 'Please pass a valid speed.'
+    expect(speed_in_mph('123')).to eq(error)
+    expect(speed_in_mph(true)).to eq(error)
+  end
+
+  it 'returns the new value in miles per hour' do
+    expect(speed_in_mph(120.3)).to eq(74.75)
+    expect(speed_in_mph(10)).to eq(6.21)
+  end
+end
